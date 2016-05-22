3ef0e7110644b501ea378e79ac3f14579d6a83b7
Mathieu Virbel <mat@kivy.org>
2016-05-22 12:46:26 +0200

    bump to 0.6

--- condiment.py
+++ condiment.py
@@ -6,7 +6,7 @@ Condiment
 Conditionally include or remove code portion, according to the environment.
 '''
 
-__version__ = '0.5.2'
+__version__ = '0.6'
 
 from os import environ, remove
 from os.path import join, dirname, basename
